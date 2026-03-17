d6d1fe5cc4889ed6377450e8daea23486d9682e0
abdumehammed <abdumehammed@gmail.com>
2026-03-17 08:48:46 +0300

    fixwd news to /news on news itmphp

--- news-items.php
+++ news-items.php
@@ -1,30 +1,32 @@
 <div class="container my-4">
     <div class="row g-4 justify-content-center">
         <div class="col-md-6">
-            <div class="card news-card">
+            <div class="card news-card">,
+                <a target="_blank" href="pdf/የሐረር_ብድርና_ቁጠባ_ተቋም_አ.ማ._አስቸኳይ_አንደኛ_መደበኛ_ጠቅላላ_ጉባኤ_ጥሪ.pdf" >
                 <img src="pdf/miyazya7.PNG" class="card-img-top news-image" alt="News 1" />
+                </a>
                 <div class="card-body">
                     <h5 class="card-title">የሐረር ብድርና ቁጠባ ተቋም አክስዮን ማህበር የባለአክሲዮኖች 1<sup>ኛ</sup> አስቸኳይ ጠቅላላ ጉባኤ ጥሪ</h5>
                     <small class="text-muted">Published on: March 16, 2026</small>
                     <p class="card-text mt-3">የሐረር ብድርና ቁጠባ ተቋም አ.ማ  የባለአክሲዮኖች አንደኛ አስቸኳይ ጠቅላላ ጉባኤ ሚያዚ 07 ቀን 2018 ዓ.ም ከጠዋቱ 3፡00 ሰዓት ላይ በሐረር ብድርና ቁጠባ ተቋም ዋና ስራ አስኪያጅ ቢሮ ስለሚካሄድ እንድትገኙ የዳይሬክተሮች ቦርድ ጥሪውን ያቀርባል፡፡</p>
                     <div class="news-tags mt-2">
-                        <span class="tag">Announcements</span>
                     </div>
-                    <a class="btn btn-primary mt-3" href="News.php" target="_blank">View full news page</a>
+                    <a class="btn btn-primary mt-3" href="news.php" target="_blank">Read More</a>
                 </div>
             </div>
         </div>
         <div class="col-md-6">
             <div class="card news-card">
+                <a target="_blank" href="pdf/የሐረር_ብድርና_ቁጠባ_ተቋም_አ.ማ._የባለአክሲዮኖች_አንደኛ_መደበኛ_ጠቅላላ_ጉባኤ_ጥሪ.pdf">
                 <img src="pdf/miyazya6.PNG" class="card-img-top news-image" alt="News 2" />
+                </a>
                 <div class="card-body">
                     <h5 class="card-title">የሐረር ብድርና ቁጠባ ተቋም አክስዮን ማህበር ባለአክሲዮኖች 1<sup>ኛ</sup> መደበኛ ጠቅላላ ጉባኤ ጥሪ</h5>
                     <small class="text-muted">Published on: March 16, 2026</small>
                     <p class="card-text mt-3">የሐረር ብድርና ቁጠባ ተቋም አ.ማ የባለአክሲዮኖች አንደኛ መደበኛ ጠቅላላ ጉባኤ ሚያዚያ 6 ቀን 2018 ዓ.ም ከጠዋቱ 3፡00 ሰዓት ላይ በሐረር ብድርና ቁጠባ ተቋም ዋና ስራ አስኪያጅ ቢሮ ስለሚካሄድ እንድትገኙ የዳይሬክተሮች ቦርድ ጥሪውን ያቀርባል፡፡</p>
                     <div class="news-tags mt-2">
-                        <span class="tag">Announcements</span>
                     </div>
-                    <a class="btn btn-primary mt-3" href="News.php" target="_blank">View full news page</a>
+                    <a class="btn btn-primary mt-3" href="news.php" target="_blank">Read More</a>
                 </div>
             </div>
         </div>
